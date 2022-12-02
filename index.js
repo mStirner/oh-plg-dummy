095cb2a487210001f0d38855c4bb78381edc95fa
mStirner <marc.stirner@hotmail.de>
2022-12-02 19:27:02 +0100

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = (info, logger, init) => {
+    return init([], (scope, []) => {
+
+        // feedback
+        console.log("Hello World", info);
+
+        // logger 
+        logger.trace("Hello from plugin:", info.name);
+        logger.debug("Hello from plugin:", info.name);
+        logger.info("Hello from plugin:", info.name);
+        logger.warn("Hello from plugin:", info.name);
+        logger.error("Hello from plugin:", info.name);
+
+    });
+};
